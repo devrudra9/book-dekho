1bd2c95d5750feb0d15e1816057d619f3a733019
Rudreshwar Baranwal <baranwalrudreshwar@gmail.com>
2023-01-07 09:48:36 +0530

    commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Modal from "./Modal";
 
+
 const Card = ({ book }) => {
     const [show, setShow] = useState(false);
     const [bookItem, setItem] = useState();
